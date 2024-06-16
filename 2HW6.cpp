2fa3cf3ec4613d2a6e03a6603a13219475ee6040
Hr-chii <calliehuang1006@gmail.com>
2024-06-17 00:20:21 +0800

    base

---
+++ 2HW6.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+class Dessert {
+private:
+    int weight;
+public:
+    Dessert() : weight(100) {}
+    ~Dessert() { cout << "Dessert destructor\n"; }
+    int getWeight() const { return weight; }
+    void eat() {
+        cout << "Eating...\n";
+        weight = (weight >= 20 ? weight - 20 : 0);
+    }
+};
+
+class Pen {
+public:
+    ~Pen() { cout << "Pen destructor\n"; }
+    void write() { cout << "Writing...\n"; }
+};
+
+template <typename T>
+class MySmartPointer {
+    T* ptr;
+public:
+    MySmartPointer(T* p = nullptr) : ptr(p) {}
+    ~MySmartPointer() { delete ptr; }
+    T& operator*() { return *ptr; }
+    T* operator->() { return ptr; }
+    MySmartPointer(const MySmartPointer&) = delete;
+    MySmartPointer& operator=(const MySmartPointer&) = delete;
+};
+
+int main() {
+    MySmartPointer<Dessert> myMealPtr(new Dessert);
+    MySmartPointer<Pen> myPenPtr(new Pen);
+    char choice;
+
+    while (cin >> choice) {
+        switch (choice) {
+            case '1':
+                (*myMealPtr).eat();
+                assert(myMealPtr->getWeight() != 100);
+                break;
+            case '2':
+                myPenPtr->write();
+                break;
+            default:
+                return 0;
+        }
+    }
+}
+
